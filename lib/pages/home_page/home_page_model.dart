50f908b814a9092a9941b0f1e9d6385c8f34533f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-29 15:31:19 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/home_page/home_page_model.dart
+++ lib/pages/home_page/home_page_model.dart
@@ -9,6 +9,7 @@ import '/flutter_flow/custom_functions.dart' as functions;
 import 'home_page_widget.dart' show HomePageWidget;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
@@ -28,11 +29,14 @@ class HomePageModel extends FlutterFlowModel<HomePageWidget> {
 
   ///  State fields for stateful widgets in this page.
 
+  final shortcutsFocusNode = FocusNode();
   // Stores action output result for [Custom Action - sortByIsSelected] action in NodeComponent widget.
   List<NodeStruct>? sortedNodes;
 
   @override
-  void initState(BuildContext context) {}
+  void initState(BuildContext context) {
+    shortcutsFocusNode.requestFocus();
+  }
 
   @override
   void dispose() {}
