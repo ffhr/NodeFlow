aaf5a8cd46be72429f275dd3e6562de840a1aa37
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-29 11:58:47 +0000

    Updating to latest FlutterFlow output.

--- lib/components/playground_i_s_stack_widget.dart
+++ lib/components/playground_i_s_stack_widget.dart
@@ -60,13 +60,13 @@ class _PlaygroundISStackWidgetState extends State<PlaygroundISStackWidget> {
             ),
           ),
           Align(
-            alignment: AlignmentDirectional(0.5, -0.35),
+            alignment: AlignmentDirectional(-0.5, 0.0),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(8.0),
               child: SvgPicture.asset(
-                'assets/images/coordinate_system_2D_math_v1.svg',
-                width: 256.0,
-                height: 256.0,
+                'assets/images/coordinate_system_2D_math_v1_a.svg',
+                width: 2211.0,
+                height: 2211.0,
                 fit: BoxFit.cover,
                 alignment: Alignment(0.0, 0.0),
               ),
