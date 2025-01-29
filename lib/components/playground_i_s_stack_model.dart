331e051f3c7dcfdba1d7c55f098fcb25838dcede
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-29 11:10:11 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/components/playground_i_s_stack_model.dart
@@ -0,0 +1,35 @@
+import '/backend/schema/structs/index.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'dart:ui';
+import '/flutter_flow/custom_functions.dart' as functions;
+import 'playground_i_s_stack_widget.dart' show PlaygroundISStackWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class PlaygroundISStackModel extends FlutterFlowModel<PlaygroundISStackWidget> {
+  ///  Local state fields for this component.
+
+  double virtualX = 0.0;
+
+  double? virtualY = 0.0;
+
+  double nodeWidth = 200.0;
+
+  double nodeHeight = 200.0;
+
+  double zoomFactor = 1.0;
+
+  NFPointStruct? viewportCenter;
+  void updateViewportCenterStruct(Function(NFPointStruct) updateFn) {
+    updateFn(viewportCenter ??= NFPointStruct());
+  }
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
