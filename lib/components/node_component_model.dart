532640eb139fdea5952456047da1b9521973c87f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-29 18:53:50 +0000

    Updating to latest FlutterFlow output.

--- lib/components/node_component_model.dart
+++ lib/components/node_component_model.dart
@@ -3,7 +3,6 @@ import '/components/socket_component_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'dart:ui';
-import '/custom_code/actions/index.dart' as actions;
 import 'node_component_widget.dart' show NodeComponentWidget;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
--- lib/components/node_component_widget.dart
+++ lib/components/node_component_widget.dart
@@ -3,7 +3,6 @@ import '/components/socket_component_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'dart:ui';
-import '/custom_code/actions/index.dart' as actions;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -191,7 +190,9 @@ class _NodeComponentWidgetState extends State<NodeComponentWidget> {
                         'Keyhbf_${inputsListIndex}_of_${inputsList.length}'),
                     nodeSocket: inputsListItem.socket,
                     onSocketTap: () async {
-                      await actions.emptyAction();
+                      // IsDrawing active
+                      FFAppState().IsDrawingActive = true;
+                      safeSetState(() {});
                     },
                   );
                 }).divide(SizedBox(height: 10.0)),
