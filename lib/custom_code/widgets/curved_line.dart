2738ec59690b38f8adcf343685b0dba0724cfa80
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-31 21:05:25 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/curved_line.dart
+++ lib/custom_code/widgets/curved_line.dart
@@ -11,6 +11,10 @@ import 'package:flutter/material.dart';
 // Begin custom widget code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
+import 'index.dart'; // Imports other custom widgets
+
+import 'dart:math';
+
 enum LineDirection {
   topLeftToBottomRight,
   topRightToBottomLeft,
@@ -123,6 +127,33 @@ class CurvedLinePainter extends CustomPainter {
     }
     return LineDirection.topLeftToBottomRight;
   }
+
+  // Hit test method for lines
+  bool hitTest(Offset position) {
+    final double tolerance = 15.0; // Define a tolerance distance
+
+    // Calculate the distance from the tap position to the line
+    final double distance = _distanceToLine(position, start, end);
+
+    return distance <= tolerance;
+  }
+
+  // Helper function to calculate the distance from a point to a line segment
+  double _distanceToLine(Offset point, Offset start, Offset end) {
+    final double dx = end.dx - start.dx;
+    final double dy = end.dy - start.dy;
+    final double lengthSquared = dx * dx + dy * dy;
+    double t = ((point.dx - start.dx) * dx + (point.dy - start.dy) * dy) /
+        lengthSquared;
+
+    t = t.clamp(0, 1);
+    final double nearestX = start.dx + t * dx;
+    final double nearestY = start.dy + t * dy;
+    final double distance = (point.dx - nearestX) * (point.dx - nearestX) +
+        (point.dy - nearestY) * (point.dy - nearestY);
+
+    return sqrt(distance);
+  }
 }
 
 class CurvedLine extends StatefulWidget {
@@ -132,26 +163,57 @@ class CurvedLine extends StatefulWidget {
     this.height,
     required this.start,
     required this.end,
+    this.onTap,
   });
 
   final double? width;
   final double? height;
   final NFPointStruct start;
   final NFPointStruct end;
+  final Future Function()? onTap;
 
   @override
   State<CurvedLine> createState() => _CurvedLineState();
 }
 
 class _CurvedLineState extends State<CurvedLine> {
+  CurvedLinePainter _painter = CurvedLinePainter(Offset(0, 0), Offset(0, 0));
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _painter = CurvedLinePainter(
+        Offset(widget.start.positionX, widget.start.positionY),
+        Offset(widget.end.positionX, widget.end.positionY));
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant CurvedLine oldWidget) {
+    _painter = CurvedLinePainter(
+        Offset(widget.start.positionX, widget.start.positionY),
+        Offset(widget.end.positionX, widget.end.positionY));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return RepaintBoundary(
-      child: CustomPaint(
-          size: MediaQuery.of(context).size,
-          painter: CurvedLinePainter(
-              Offset(widget.start.positionX, widget.start.positionY),
-              Offset(widget.end.positionX, widget.end.positionY))),
-    );
+    return GestureDetector(
+        onTapUp: (details) {
+          final RenderBox renderBox = context.findRenderObject() as RenderBox;
+          final Offset localPosition =
+              renderBox.globalToLocal(details.globalPosition);
+          if (_painter.hitTest(localPosition)) {
+            //print('Tapped on rendered part!');
+            widget.onTap?.call();
+          } else {
+            //print('Tapped outside rendered part.');
+          }
+        },
+        child: RepaintBoundary(
+          child: CustomPaint(
+            size: MediaQuery.of(context).size,
+            painter: _painter,
+          ),
+        ));
   }
 }
