ad8aeb077e6faa4edaa4fb9cf3326d138478e32c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-31 16:45:48 +0000

    Updating to latest FlutterFlow output.

--- lib/components/socket_component_widget.dart
+++ lib/components/socket_component_widget.dart
@@ -51,6 +51,12 @@ class _SocketComponentWidgetState extends State<SocketComponentWidget> {
     super.initState();
     _model = createModel(context, () => SocketComponentModel());
 
+    // On component load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      _model.isClicked = widget!.isClicked;
+      safeSetState(() {});
+    });
+
     WidgetsBinding.instance.addPostFrameCallback((_) => safeSetState(() {}));
   }
 
@@ -70,13 +76,16 @@ class _SocketComponentWidgetState extends State<SocketComponentWidget> {
       cursor: MouseCursor.defer ?? MouseCursor.defer,
       child: Builder(
         builder: (context) {
-          if (widget!.isClicked) {
+          if (_model.isClicked) {
             return GestureDetector(
               onTap: () async {
                 // onClicked(false)
                 await widget.onClicked?.call(
                   false,
                 );
+                // isClicked=false
+                _model.isClicked = false;
+                safeSetState(() {});
               },
               onPanDown: (details) async {
                 if ((FFAppState().EdgeDrawing.drawingState ==
@@ -150,6 +159,9 @@ class _SocketComponentWidgetState extends State<SocketComponentWidget> {
                 await widget.onClicked?.call(
                   true,
                 );
+                // isClicked=true
+                _model.isClicked = true;
+                safeSetState(() {});
               },
               onPanDown: (details) async {
                 if ((FFAppState().EdgeDrawing.drawingState ==
