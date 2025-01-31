93066ae651b0044fa2a38d0d6fa95f8b96f28ec7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-31 17:33:24 +0000

    Updating to latest FlutterFlow output.

--- lib/components/node_component_widget.dart
+++ lib/components/node_component_widget.dart
@@ -304,7 +304,7 @@ class _NodeComponentWidgetState extends State<NodeComponentWidget> {
                                   .hasTargetNodeId()) {
                             // Unset
                             FFAppState().CurrentBuildingEdge = NodeEdgeStruct();
-                            FFAppState().update(() {});
+                            _model.updatePage(() {});
                             ScaffoldMessenger.of(context).showSnackBar(
                               SnackBar(
                                 content: Text(
@@ -398,7 +398,7 @@ class _NodeComponentWidgetState extends State<NodeComponentWidget> {
                               // Unset
                               FFAppState().CurrentBuildingEdge =
                                   NodeEdgeStruct();
-                              FFAppState().update(() {});
+                              _model.updatePage(() {});
                               ScaffoldMessenger.of(context).showSnackBar(
                                 SnackBar(
                                   content: Text(
