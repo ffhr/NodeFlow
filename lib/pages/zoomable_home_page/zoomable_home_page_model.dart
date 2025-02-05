7c0e853d29ddce0b82f1780fb6a4b4fcf6ffeb7e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-05 20:39:18 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/zoomable_home_page/zoomable_home_page_model.dart
+++ lib/pages/zoomable_home_page/zoomable_home_page_model.dart
@@ -1,4 +1,4 @@
-import '/components/n_f_diagram_widget.dart';
+import '/components/n_f_diagram_min_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
--- lib/pages/zoomable_home_page/zoomable_home_page_widget.dart
+++ lib/pages/zoomable_home_page/zoomable_home_page_widget.dart
@@ -1,4 +1,4 @@
-import '/components/n_f_diagram_widget.dart';
+import '/components/n_f_diagram_min_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
@@ -54,7 +54,7 @@ class _ZoomableHomePageWidgetState extends State<ZoomableHomePageWidget> {
             child: custom_widgets.ZoomableStack(
               width: double.infinity,
               height: double.infinity,
-              stackComponent: () => NFDiagramWidget(),
+              stackComponent: () => NFDiagramMinWidget(),
             ),
           ),
         ),
