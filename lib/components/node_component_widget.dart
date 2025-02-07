9c89c921275e4ff4fa9c26058949f0b1042838a0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-07 18:27:37 +0000

    Updating to latest FlutterFlow output.

--- lib/components/node_component_widget.dart
+++ lib/components/node_component_widget.dart
@@ -71,84 +71,65 @@ class _NodeComponentWidgetState extends State<NodeComponentWidget> {
       height: widget!.node?.size?.height,
       child: Stack(
         children: [
-          Padding(
-            padding: EdgeInsetsDirectional.fromSTEB(15.0, 0.0, 15.0, 0.0),
-            child: GestureDetector(
-              onPanDown: (details) async {
-                if ((FFAppState().EdgeDrawing.drawingState ==
-                        DrawingState.inactive) ||
-                    (FFAppState().EdgeDrawing.drawingState ==
-                        DrawingState.finished)) {
-                  // On pan down
-                  await widget.onPanDown?.call();
-                }
-              },
-              onPanEnd: (details) async {
-                if ((FFAppState().EdgeDrawing.drawingState ==
-                        DrawingState.inactive) ||
-                    (FFAppState().EdgeDrawing.drawingState ==
-                        DrawingState.finished)) {
-                  // On pan end
-                  await widget.onPanEnd?.call();
-                }
-              },
-              onPanUpdate: (details) async {
-                if ((FFAppState().EdgeDrawing.drawingState ==
-                        DrawingState.inactive) ||
-                    (FFAppState().EdgeDrawing.drawingState ==
-                        DrawingState.finished)) {
-                  // On pan update
-                  await widget.onPanUpdate?.call(
-                    NFPointStruct(
-                      positionX: details.delta.dx,
-                      positionY: details.delta.dy,
-                    ),
-                  );
-                }
-              },
-              onTapDown: (details) async {
-                if ((FFAppState().EdgeDrawing.drawingState ==
-                        DrawingState.inactive) ||
-                    (FFAppState().EdgeDrawing.drawingState ==
-                        DrawingState.finished)) {
-                  // On tap down
-                  await widget.onTapDown?.call();
-                }
-              },
-              onTapUp: (details) async {
-                if ((FFAppState().EdgeDrawing.drawingState ==
-                        DrawingState.inactive) ||
-                    (FFAppState().EdgeDrawing.drawingState ==
-                        DrawingState.finished)) {
-                  // On tap up
-                  await widget.onTapUp?.call();
-                }
-              },
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
-                child: Container(
-                  width: widget!.node?.size?.width,
-                  height: widget!.node?.size?.height,
-                  decoration: BoxDecoration(
-                    color: FlutterFlowTheme.of(context).secondary,
-                    borderRadius: BorderRadius.circular(10.0),
-                    border: Border.all(
-                      color: widget!.node!.isSelected
-                          ? Colors.white
-                          : Color(0x00000000),
-                      width: 2.0,
-                    ),
-                  ),
-                  child: Visibility(
-                    visible: widget!.child != null,
-                    child: Builder(builder: (_) {
-                      return widget.child!();
-                    }),
-                  ),
-                ),
+          if (widget!.child != null)
+            Align(
+              alignment: AlignmentDirectional(0.0, 0.0),
+              child: GestureDetector(
+                onPanDown: (details) async {
+                  if ((FFAppState().EdgeDrawing.drawingState ==
+                          DrawingState.inactive) ||
+                      (FFAppState().EdgeDrawing.drawingState ==
+                          DrawingState.finished)) {
+                    // On pan down
+                    await widget.onPanDown?.call();
+                  }
+                },
+                onPanEnd: (details) async {
+                  if ((FFAppState().EdgeDrawing.drawingState ==
+                          DrawingState.inactive) ||
+                      (FFAppState().EdgeDrawing.drawingState ==
+                          DrawingState.finished)) {
+                    // On pan end
+                    await widget.onPanEnd?.call();
+                  }
+                },
+                onPanUpdate: (details) async {
+                  if ((FFAppState().EdgeDrawing.drawingState ==
+                          DrawingState.inactive) ||
+                      (FFAppState().EdgeDrawing.drawingState ==
+                          DrawingState.finished)) {
+                    // On pan update
+                    await widget.onPanUpdate?.call(
+                      NFPointStruct(
+                        positionX: details.delta.dx,
+                        positionY: details.delta.dy,
+                      ),
+                    );
+                  }
+                },
+                onTapDown: (details) async {
+                  if ((FFAppState().EdgeDrawing.drawingState ==
+                          DrawingState.inactive) ||
+                      (FFAppState().EdgeDrawing.drawingState ==
+                          DrawingState.finished)) {
+                    // On tap down
+                    await widget.onTapDown?.call();
+                  }
+                },
+                onTapUp: (details) async {
+                  if ((FFAppState().EdgeDrawing.drawingState ==
+                          DrawingState.inactive) ||
+                      (FFAppState().EdgeDrawing.drawingState ==
+                          DrawingState.finished)) {
+                    // On tap up
+                    await widget.onTapUp?.call();
+                  }
+                },
+                child: Builder(builder: (_) {
+                  return widget.child!();
+                }),
               ),
             ),
-          ),
           Padding(
             padding: EdgeInsetsDirectional.fromSTEB(5.0, 0.0, 0.0, 0.0),
             child: Builder(
